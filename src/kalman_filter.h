defb1787ea41e2c1e42673df10221e6ab8be299b
Sergey Morozov <sergey.morozov@emc.com>
2017-10-09 11:48:14 +0300

    README updated

--- src/kalman_filter.h
+++ src/kalman_filter.h
@@ -39,12 +39,13 @@ public:
 
   /**
    * Init Initializes Kalman filter
-   * @param x_in Initial state
-   * @param P_in Initial state covariance
-   * @param F_in Transition matrix
-   * @param H_in Measurement matrix
-   * @param R_in Measurement covariance matrix
-   * @param Q_in Process covariance matrix
+   * @param x_in        Initial state
+   * @param P_in        Initial state covariance
+   * @param F_in        Transition matrix
+   * @param H_in        Measurement matrix
+   * @param R_lidar_in  Measurement covariance matrix for lidar
+   * @param R_radar_in  Measurement covariance matrix for radar
+   * @param Q_in        Process covariance matrix
    */
   void Init(Eigen::VectorXd &x_in, Eigen::MatrixXd &P_in, Eigen::MatrixXd &F_in,
       Eigen::MatrixXd &H_in, Eigen::MatrixXd &R_lidar_in, Eigen::MatrixXd &R_radar_in, Eigen::MatrixXd &Q_in);
